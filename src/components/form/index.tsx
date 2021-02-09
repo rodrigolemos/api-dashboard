83c3e5d352f09c30dd4deaebe465844fbc2a142e
Rodrigo Lemos <rodrigolemosrl@hotmail.com>
2021-02-09 18:01:18 -0300

    Utilização de react-select

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,8 +1,27 @@
+import Select from 'react-select';
+
 import { FormWrapper } from './styles';
 
 const Form: React.FC = (): React.ReactElement => {
+  const options = [
+    { value: 1, label: 'Primeira API' },
+    { value: 2, label: 'Segunda API' },
+    { value: 3, label: 'Terceira API' }
+  ]
+
   return (
-    <FormWrapper>form</FormWrapper>
+    <FormWrapper>
+      <h1>Logs</h1>
+      <form>
+        <div>
+          <label htmlFor="api-name">Selecione a API:</label>
+          <Select
+            id="api-name"
+            options={options}
+          />
+        </div>
+      </form>
+    </FormWrapper>
   )
 }
 
--- src/components/form/styles.ts
+++ src/components/form/styles.ts
@@ -1,5 +1,28 @@
 import styled from 'styled-components';
 
-export const FormWrapper = styled.form`
+export const FormWrapper = styled.aside`
   grid-area: form;
+  flex-direction: column;
+
+  form {
+    width: 90%;
+
+    & > div {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+
+      margin: 10px;
+
+      label {
+        width: 30%;
+        text-align: right;
+        margin-right: 10px;
+      }
+
+      #api-name {
+        width: 100%;
+      }
+    }
+  }
 `;
