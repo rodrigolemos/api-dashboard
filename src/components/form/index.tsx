9f1f9d08d54ab0607c5acd6219656efd7bb1f097
Rodrigo Lemos <rodrigolemosrl@hotmail.com>
2021-02-10 16:36:21 -0300

    Filtros adicionais inicial

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,27 +1,49 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Select from 'react-select';
 
 import { FormWrapper } from './styles';
 
-interface IApis {
-  value: number;
+interface ISelect {
+  value: number | string;
   label: string;
 }
 
 const Form: React.FC = (): React.ReactElement => {
-  const [apis, setApis] = useState<IApis[]>([]);
+  const [apis, setApis] = useState<ISelect[]>([]);
+  const [additionalFilterOptions, setAdditionalFilterOptions] = useState<ISelect[]>([
+    { value: 'header_req', label: 'Header de requisição' },
+    { value: 'body_req', label: 'Body de requisição' },
+    { value: 'header_res', label: 'Header de resposta' },
+    { value: 'body_res', label: 'Body de resposta' },
+    { value: 'code_res', label: 'Código de resposta' },
+  ]);
+  const [additionalFilters, setAdditionalFilters] = useState<ISelect[]>([]);
 
-  const fetchApis = (): void => {
+  const handleAddAdditionalFilter = (selectedOption: any) => {
+    setAdditionalFilters([...additionalFilters, selectedOption])
+    setAdditionalFilterOptions(
+      additionalFilterOptions.filter(additionalFilterOption => additionalFilterOption.value !== selectedOption.value)
+    )
+  }
+
+  const handleRemoveAdditionalFilter = (removeAdditional: ISelect) => {
+    setAdditionalFilterOptions([...additionalFilterOptions, removeAdditional])
+    setAdditionalFilters(
+      additionalFilters.filter(additionalFilters => additionalFilters.value !== removeAdditional.value)
+    )
+  }
+
+  const fetchApis = useCallback((): void => {
     setApis([
       { value: 1, label: 'Primeira API' },
       { value: 2, label: 'Segunda API' },
       { value: 3, label: 'Terceira API' }
     ])
-  }
+  }, [])
 
   useEffect(() => {
     fetchApis()
-  }, [])
+  }, [fetchApis])
 
   return (
     <FormWrapper>
@@ -29,36 +51,61 @@ const Form: React.FC = (): React.ReactElement => {
       <form>
 
         <div className="row">
-
           <div className="col-full">
-            <label htmlFor="name">Selecione a API</label>
+            <label htmlFor="name">Serviço</label>
             <Select
               id="name"
               options={apis}
               required
             />
           </div>
-
         </div>
 
         <div className="row">
-
           <div className="col-half">
             <label htmlFor="date">Data</label>
-            <input type="date" id="date" defaultValue={new Date().toISOString().split('T')[0]} />
+            <input required type="date" id="date" defaultValue={new Date().toISOString().split('T')[0]} />
           </div>
-
           <div className="col-half">
             <div className="col-half">
               <label htmlFor="time-begin">Hora início</label>
-              <input type="time" id="time-begin" defaultValue={'00:00'} />
+              <input required type="time" id="time-begin" defaultValue={'00:00'} />
             </div>
             <div className="col-half">
               <label htmlFor="time-end">Hora fim</label>
-              <input type="time" id="time-end" defaultValue={'23:59'} />
+              <input required type="time" id="time-end" defaultValue={'23:59'} />
             </div>
           </div>
+        </div>
 
+        <div className="row">
+          <div className="col-full">
+            <label htmlFor="add-filter">Filtros adicionais</label>
+            <Select
+              id="add-filter"
+              options={additionalFilterOptions}
+              onChange={handleAddAdditionalFilter}
+            />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-full">
+            <ul>
+              {additionalFilters.map((additionalFilter: ISelect) => (
+                <li key={additionalFilter.value}>
+                  <input type="text" required className="additional" placeholder={additionalFilter.label} />
+                  <button className="secondary" onClick={() => handleRemoveAdditionalFilter(additionalFilter)}>Remover</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-full centered">
+            <button type="submit" className="primary">Consultar</button>
+          </div>
         </div>
 
       </form>
--- src/components/form/styles.ts
+++ src/components/form/styles.ts
@@ -4,25 +4,80 @@ export const FormWrapper = styled.aside`
   grid-area: form;
   flex-direction: column;
 
+  h1 {
+    font-size: 34px;
+    font-weight: normal;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 10px;
+  }
+
+  input {
+    padding: 9px;
+    font-size: 14px;
+    border: 1px solid #CCC;
+    border-radius: 5px;
+    outline: none;
+    transition: border .1s ease-in-out;
+
+    &:focus, &:active {
+      border: 1px solid #2684FF;
+    }
+
+    &.additional {
+      width: 75%;
+    }
+  }
+
+  button.primary {
+    border: none;
+    padding: 12px 20px;
+    border-radius: 5px;
+    background-color: #2684FF;
+    color: #FFF;
+    font-size: 16px;
+    font-weight: bold;
+    cursor: pointer;
+    transition: all .2s ease-in-out;
+
+    &:hover {
+      transform: translateY(-2px);
+      box-shadow: 0px 5px 5px #CCC;
+    }
+  }
+
+  button.secondary {
+    border: none;
+    padding: 8px 15px;
+    margin: 0 10px;
+    border-radius: 5px;
+    background-color: #E2E8F0;
+    color: #2D3748;
+    font-size: 16px;
+    font-weight: bold;
+    cursor: pointer;
+    transition: all .2s ease-in-out;
+
+    &:hover {
+      box-shadow: 0px 5px 5px #CCC;
+    }
+  }
+
   form {
     width: 90%;
     display: flex;
     flex-direction: column;
 
-    label {
-      display: block;
-      margin-bottom: 10px;
-    }
+    ul {
+      margin: 0;
+      padding: 0;
 
-    input {
-      padding: 9px;
-      font-size: 14px;
-      border: 1px solid #CCC;
-      border-radius: 5px;
-    }
-
-    #name {
-      width: 100%;
+      li {
+        list-style: none;
+        margin: 10px 0px;
+      }
     }
 
     .row {
@@ -36,6 +91,12 @@ export const FormWrapper = styled.aside`
       .col-half {
         display: inline-block;
         width: 50%
+      }
+
+      .centered {
+        display: flex;
+        justify-content: center;
+        align-items: center;
       }
 
     }
