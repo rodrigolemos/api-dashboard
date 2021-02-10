775bf07bb070028d9698fff8b25b181881d60a77
Rodrigo Lemos <rodrigolemosrl@hotmail.com>
2021-02-10 10:51:04 -0300

    Select de APIs

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,23 +1,37 @@
+import { useEffect, useState } from 'react';
 import Select from 'react-select';
 
 import { FormWrapper } from './styles';
 
+interface IApis {
+  value: number;
+  label: string;
+}
+
 const Form: React.FC = (): React.ReactElement => {
-  const options = [
-    { value: 1, label: 'Primeira API' },
-    { value: 2, label: 'Segunda API' },
-    { value: 3, label: 'Terceira API' }
-  ]
+  const [apis, setApis] = useState<IApis[]>([]);
+
+  const fetchApis = (): void => {
+    setApis([
+      { value: 1, label: 'Primeira API' },
+      { value: 2, label: 'Segunda API' },
+      { value: 3, label: 'Terceira API' }
+    ])
+  }
+
+  useEffect(() => {
+    fetchApis()
+  }, [])
 
   return (
     <FormWrapper>
-      <h1>Logs</h1>
+      <h1>APIs IntergrALL</h1>
       <form>
         <div>
           <label htmlFor="api-name">Selecione a API:</label>
           <Select
             id="api-name"
-            options={options}
+            options={apis}
           />
         </div>
       </form>
